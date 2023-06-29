f3ed4d2e1ac738e232faea106b9ebb26e67b908c
Igor Lee <srleehbb@gmail.com>
2023-06-29 13:09:22 -0300

    Mostra a validação do formulário em tela

--- js/classes/formPrinter.js
+++ js/classes/formPrinter.js
@@ -3,6 +3,7 @@ export default class FormPrinter {
     this.formId = formId;
     this.form = document.getElementById(formId);
     this.printButton = this.form.querySelector('button[type="submit"]');
+    this.errors = {};
 
     this.printButton.addEventListener('click', this.handlePrint.bind(this));
   }
@@ -44,82 +45,83 @@ export default class FormPrinter {
     try {
       this.validateForm(organizedFormValues);
       console.log(organizedFormValues);
-      //Vai para parte de impressão após todos os dados validados!
+
+      //Chama o método para gerar o arquivo de dados
+
     } catch (error) {
-      //Adiciona as classes de validação para os inputs
       console.error(error.message);
     }
   }
 
   validateForm(formValues) {
-    const errors = {};
+    this.errors = {};
 
     // Validar Cliente
     if (!formValues.client || typeof formValues.client !== 'string') {
-      errors.client = 'O campo Cliente é obrigatório';
+      this.errors.client = 'O campo Cliente é obrigatório';
     }
 
     // Validar ID do Cliente
     const idClient = formValues['id-client'];
     if (idClient && !/^(\d{3}\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})$/.test(idClient)) {
-      errors['id-client'] = 'O CPF ou CNPJ digitado não é válido.';
+      this.errors['id-client'] = 'O CPF ou CNPJ digitado não é válido.';
     }
 
     // Validar Whatsapp
     const whatsapp = formValues.whatsapp;
     if (whatsapp && !/^(\+\d{1,3})?\s*\(?(\d{2})\)?\s*\d{4,5}\-?\d{4}$/.test(whatsapp)) {
-      errors.whatsapp = 'O Whatsapp digitado possui um formato inválido.';
+      this.errors.whatsapp = 'O Whatsapp digitado possui um formato inválido.';
     }
 
     // Validar Email
     const email = formValues.email;
     if (email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      errors.email = 'O Email digitado possui um formato inválido.';
+      this.errors.email = 'O Email digitado possui um formato inválido.';
     }
 
     //valida Endereço
     if (typeof formValues.address !== 'string') {
-      errors.address = 'O Endereço digitado parece estranho.';
+      this.errors.address = 'O Endereço digitado parece estranho.';
     }
 
     // Validar Número
     const number = formValues['address-number'];
     if (number && !/^(s\/n|\d+)$/.test(number)) {
-      errors['address-number'] = 'Apenas "s/n" ou números.';
+      this.errors['address-number'] = 'Apenas "s/n" ou números.';
     }
 
     //valida Bairro
     if (typeof formValues.neighborhood !== 'string') {
-      errors.neighborhood = 'O Bairro digitado parece estranho.';
+      this.errors.neighborhood = 'O Bairro digitado parece estranho.';
     }
 
     //valida Complemento
     if (typeof formValues.complement !== 'string') {
-      errors.complement = 'O Complemento digitado parece estranho.';
+      this.errors.complement = 'O Complemento digitado parece estranho.';
     }
 
     // Validar Cidade
     if (!formValues.city || typeof formValues.city !== 'string') {
-      errors.city = 'O campo Cidade é obrigatório';
+      this.errors.city = 'O campo Cidade é obrigatório';
     }
 
     // Validar Cidade
     if (!formValues.state || typeof formValues.state !== 'string') {
-      errors.state = 'O campo Estado é obrigatório';
+      this.errors.state = 'O campo Estado é obrigatório';
     }
 
     // Validar CEP
     const cep = formValues['cep'];
     if (cep && !/^\d{5}-\d{3}$/.test(cep)) {
-      errors['cep'] = 'O CEP possui um formato inválido';
+      this.errors['cep'] = 'O CEP possui um formato inválido';
     }
 
     // Validar ID do Pedido
     const idPedido = formValues['idOrder'];
     if (!idPedido) {
-      errors['idOrder'] = 'O ID do Pedido é obrigatório.';
+      this.errors['idOrder'] = 'O ID do Pedido é obrigatório.';
     } else if(idPedido.length !== 4 || isNaN(idPedido)){
-      errors['idOrder'] = 'O ID do Pedido deve ter 4 dígitos.';
+      this.errors['idOrder'] = 'O ID do Pedido deve ter 4 dígitos.';
     }
 
     // Validar os Itens
@@ -133,33 +135,33 @@ export default class FormPrinter {
 
       //Validar Item
       if (!nameItem || typeof nameItem !== 'string') {
-        errors[`item-${itemId}`] = 'O item é obrigatório.';
+        this.errors[`item-${itemId}`] = 'O item é obrigatório.';
       }
 
       //Validar Quantidade
       if (!amountItem || amountItem === 0) {
-        errors[`amount-item-${itemId}`] = 'A quantidade é obrigatória.';
+        this.errors[`amount-item-${itemId}`] = 'A quantidade é obrigatória.';
       }
 
       //Validar Valor Unitário
       if (!unitaryItem || unitaryItem === '0,00') {
-        errors[`unitary-item-${itemId}`] = 'O valor unitário é obrigatório.';
+        this.errors[`unitary-item-${itemId}`] = 'O valor unitário é obrigatório.';
       } else {
         const regex = /^\d{1,3}(?:\.\d{3})*(?:,\d{2})?$/;
 
         if (!regex.test(unitaryItem)) {
-          errors[`unitary-item-${itemId}`] = 'Formato incorreto.';
+          this.errors[`unitary-item-${itemId}`] = 'Formato incorreto.';
         }
       }
 
       //Validar Valor Total
       if (!totalItem || totalItem === '0,00') {
-        errors[`total-item-${itemId}`] = 'O valor total é obrigatório.';
+        this.errors[`total-item-${itemId}`] = 'O valor total é obrigatório.';
       } else {
         const regex = /^\d{1,3}(?:\.\d{3})*(?:,\d{2})?$/;
 
         if (!regex.test(totalItem)) {
-          errors[`total-item-${itemId}`] = 'Formato incorreto.';
+          this.errors[`total-item-${itemId}`] = 'Formato incorreto.';
         }
       }
     }
@@ -167,35 +169,69 @@ export default class FormPrinter {
     // Validar Instalação
     const installationItem = formValues['installation-item'];
     if (!installationItem && installationItem !== 0) {
-      errors['installation-item'] = 'A taxa de instalação é obrigatória.';
+      this.errors['installation-item'] = 'A taxa de instalação é obrigatória.';
     }
 
     // Validar Entrega
     const deliveryItem = formValues['delivery-item'];
     if (!deliveryItem && deliveryItem !== 0) {
-      errors['delivery-item'] = 'A taxa de entrega é obrigatória.';
+      this.errors['delivery-item'] = 'A taxa de entrega é obrigatória.';
     }
 
     // Validar Desconto
     const discountItem = formValues['discount-item'];
     if (!discountItem && discountItem !== 0) {
-      errors['discount-item'] = 'A taxa de entrega é obrigatória.';
+      this.errors['discount-item'] = 'A taxa de entrega é obrigatória.';
     }
 
     // Validar Final
     const finalItem = formValues['final-item'];
     if (!finalItem && finalItem !== 0) {
-      errors['final-item'] = 'O valor final é obrigatória.';
+      this.errors['final-item'] = 'O valor final é obrigatória.';
     }else{
       const regex = /^\d{1,3}(?:\.\d{3})*(?:,\d{2})?$/;
         if (!regex.test(finalItem)) {
-          errors['final-item'] = 'Formato incorreto.';
+          this.errors['final-item'] = 'Formato incorreto.';
         }
     }
 
-    if (Object.keys(errors).length > 0) {
+    if (Object.keys(this.errors).length > 0) {
+      this.addValidationClasses();
+      // Adiciona as classes de validação para os inputs inválidos
+      for (const key in this.errors) {
+        const input = this.form.querySelector(`[name="${key}"]`);
+        const parentDiv = input.parentElement;
+        parentDiv.classList.add('has-validation');
+        parentDiv.classList.add('has-error');
+
+        // Verifica se já existe uma div de feedback de validação
+        let feedbackDiv = parentDiv.querySelector('.invalid-feedback');
+        if (!feedbackDiv) {
+          feedbackDiv = document.createElement('div');
+          feedbackDiv.classList.add('invalid-feedback');
+          parentDiv.appendChild(feedbackDiv);
+        }
+
+        // Define a mensagem de erro na div de feedback de validação
+        feedbackDiv.innerText = this.errors[key];
+      }
       throw new Error('O formulário contém erros. Por favor, verifique os campos destacados.');
+    }else {
+      this.addValidationClasses();
     }
+  }
+
+  addValidationClasses() {
+    const inputs = this.form.querySelectorAll('input, select');
+    inputs.forEach((input) => {
+      const fieldName = input.name;
+      const isValid = !(fieldName in this.errors);
+      const classToAdd = isValid ? 'is-valid' : 'is-invalid';
+      const classToRemove = isValid ? 'is-invalid' : 'is-valid';
+
+      input.classList.add(classToAdd);
+      input.classList.remove(classToRemove);
+    });
   }
 
   createDataFile(formValues) {
